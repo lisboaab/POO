e25fef034ad53de5df281e9abb2ed04e09177b88
Beatriz Lisboa <btrzlisboa037@gmail.com>
2023-03-23 09:46:58 +0000

    exs 3

---
+++ ficha02/ex03/ex03a.js
@@ -0,0 +1,11 @@
+function min(num1, num2){
+    if (num1<num2){
+        alert(`O número ${num1} é menor`)
+    }
+    else {
+        alert(`O número ${num2} é menor`)
+    }
+}
+
+min(1,2)
+min(5,5)

e45555d62cfa780ef9c52af91265a3392c7ee9ef
Beatriz Lisboa <btrzlisboa037@gmail.com>
2023-06-03 10:36:38 +0100

    adc random info base código

--- base codigo/random.js
+++ base codigo/random.js
@@ -6,3 +6,14 @@ let randomNumber = Math.floor(Math.random()*(100 + 1));
 
 // gera um numero random entre 1 e 10
 let randomNumber2 = Math.floor(Math.random() * 10) + 1; // o 1 no final e pra somar caso o math.random de um numero 0,2 por exemplo
+
+
+// random obj de array
+const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
+
+function getRandomLetter() {
+    const randomIndex = Math.floor(Math.random() * letras.length); // Gera um índice aleatório
+    const randomLetter = letras[randomIndex]; // Obtém a letra correspondente ao índice
+    letras.splice(randomIndex, 1); // Remove a letra do array original
+    return randomLetter;
+}
